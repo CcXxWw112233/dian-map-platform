2b2ffe4fe31a6323bbd16a73e922f667b676e518
huangxd <huangxd@new-di.com>
2020-04-20 19:26:32 +0800

    :art: 左侧面板新增tabs

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,7 +4,7 @@ import { Input } from 'antd'
 export default class Search extends PureComponent {
   render() {
     return (
-      <Input.Group compact style={{ width: 328, margin:'24px 16px'}}>
+      <Input.Group compact style={{ width: 328, margin:'24px 16px 16px 16px'}}>
         <Input style={{ width: '20%',cursor:'pointer',readonly:'readonly', height:32 }} defaultValue="深圳" />
         <Input.Search style={{ width: '80%', height:32 }} defaultValue="搜索资料或目的地" />
       </Input.Group>
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,127 +1,135 @@
-import React from 'react';
-import { connect } from 'dva';
-import styles from './IndexPage.css';
-import 'antd/dist/antd.css'
-import LayerMap from '../components/maps'
-import ChangeBaseMap from '../components/subComponents/ChangeBaseMap/index'
-import { ChangeMap } from '../utils/utils'
-import PublicTools from '../components/subComponents/PublicTools/index'
+import React from "react";
+import { connect } from "dva";
+import styles from "./IndexPage.css";
+import "antd/dist/antd.css";
+import LayerMap from "../components/maps";
+import { ChangeMap } from "../utils/utils";
 
-import { getMyPosition } from '../utils/getMyPosition'
-import axios from '../services/index'
+import { getMyPosition } from "../utils/getMyPosition";
+import { Tabs } from "antd";
+import {
+  ToolBar,
+  Location,
+  LayerControl,
+  Sider,
+  Search,
+  CityPanel,
+} from "components/index";
 
-import { ToolBar, Location, LayerControl, Sider, Search, CityPanel } from 'components/index'
-
-
-
-
-
-class IndexPage extends React.Component{
-  constructor(props){
+class IndexPage extends React.Component {
+  constructor(props) {
     super(...arguments);
     this.map = null;
-    this.view = null ;
-    this.mySelfIcon = false ;
-    this.positionTimer = null ;
+    this.view = null;
+    this.mySelfIcon = false;
+    this.positionTimer = null;
     // this.state = {
     //   draw_
     // }
-
   }
   state = {
     visible: true,
-    placement: 'left',
-    left: '0px',
-    draw_visible: false
-    }
+    placement: "left",
+    left: "0px",
+    draw_visible: false,
+  };
 
   showDrawer = () => {
-    const { draw_visible } = this.state
+    const { draw_visible } = this.state;
     this.setState({
-      draw_visible: !draw_visible
-    })
-  }
+      draw_visible: !draw_visible,
+    });
+  };
   onClose = () => {
     this.setState({
       visible: false,
     });
-  }
-  componentDidMount(){
-
-  }
+  };
+  componentDidMount() {}
   // 地图加载完成
-  MapOnload = ({map,view})=>{
+  MapOnload = ({ map, view }) => {
     this.map = map;
     this.view = view;
-    this.setCenter(map,view);
-  }
+    this.setCenter(map, view);
+  };
 
   // 通过高德地图获得自己的定位
-  getMyCenter = (flag)=>{
+  getMyCenter = (flag) => {
     // 获取定位
-    getMyPosition.getPosition().then(val => {
+    getMyPosition.getPosition().then((val) => {
       // let coor = [114.11533,23.66666]
       // 转换地理坐标EPSG:4326 到 EPSG:3857
       let coordinate = getMyPosition.transformPosition(val);
       // 将视图平移到坐标中心点
-      getMyPosition.setViewCenter(coordinate,200)
-    })
-  }
+      getMyPosition.setViewCenter(coordinate, 200);
+    });
+  };
 
-  setCenter = ()=>{
+  setCenter = () => {
     // 渲染icon等;
     getMyPosition.drawPosition();
     // 启动监听--移动端才启用监听
-    if(AMap.Browser.mobile){
-      alert('当前是手机端页面，将启动移动位置更新')
+    if (AMap.Browser.mobile) {
+      alert("当前是手机端页面，将启动移动位置更新");
       this.addWatchPosition();
     }
-  }
+  };
 
   // 监听移动端的位置变化
   addWatchPosition = () => {
-    this.positionTimer = setTimeout(()=>{
-      getMyPosition.getPosition().then(obj => {
+    this.positionTimer = setTimeout(() => {
+      getMyPosition.getPosition().then((obj) => {
         let coordinate = getMyPosition.transformPosition(obj);
         // 改变自身的位置
         getMyPosition.setPosition(obj);
-        getMyPosition.setViewCenter(coordinate,200);
+        getMyPosition.setViewCenter(coordinate, 200);
         // 改变自身的精准范围
         getMyPosition.setPositionRadius(obj.accuracy);
-      })
+      });
       // 无限回调监听位置
       this.addWatchPosition();
-    },5 * 1000)
-  }
+    }, 5 * 1000);
+  };
 
   // 切换底图
-  changeMap = (val,showkeys) => {
-    let map = this.map ;
+  changeMap = (val, showkeys) => {
+    let map = this.map;
     let layers = map.getLayers();
     // 进行切换
-    ChangeMap(val,layers,map,showkeys);
-  }
-  render(){
+    ChangeMap(val, layers, map, showkeys);
+  };
+  render() {
+    const { TabPane } = Tabs;
     return (
       <div className={styles.normal}>
         {/* 地图主体 */}
-        <LayerMap onLoad={this.MapOnload}/>
+        <LayerMap onLoad={this.MapOnload} />
         {/* 切换底图组件 */}
         {/* <ChangeBaseMap onChange={this.changeMap}/> */}
         <LayerControl></LayerControl>
         {/* 工具栏 */}
         <ToolBar></ToolBar>
         <Location></Location>
-        <Sider width={360} >
+        <Sider width={360}>
           <Search></Search>
+          <Tabs defaultActiveKey="2" tabBarGutter={60}>
+            <TabPane tab={<span>项目踏勘</span>} key="1">
+              项目踏勘
+            </TabPane>
+            <TabPane tab={<span>公共数据</span>} key="2">
+              公共数据
+            </TabPane>
+            <TabPane tab={<span>远程协作</span>} key="3">
+              远程协作
+            </TabPane>
+          </Tabs>
         </Sider>
         {/* <CityPanel></CityPanel> */}
       </div>
-    )
+    );
   }
 }
 
-IndexPage.propTypes = {
-};
+IndexPage.propTypes = {};
 
-export default connect(({maps})=>({maps}))(IndexPage);
+export default connect(({ maps }) => ({ maps }))(IndexPage);
