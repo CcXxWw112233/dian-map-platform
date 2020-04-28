c79f9c6dad441c96b6497f33586ad44263f3bb68
huangxd <huangxd@new-di.com>
2020-04-28 19:59:53 +0800

    :art: 项目踏勘

--- src/pages/ProjectScouting/ScoutingDetails.js
+++ src/pages/ProjectScouting/ScoutingDetails.js
@@ -2,44 +2,152 @@ import React, { PureComponent } from "react";
 import globalStyle from "../../globalSet/styles/globalStyles.less";
 import styles from "./ScoutingDetails.less";
 import { connect } from "dva";
-const GoBack = ({ cb }) => {
-  return (
-    <i
-      className={
-        globalStyle.global_icon + ` ${styles.gobackBtn} ${globalStyle.btn}`
-      }
-      style={{
-        position: "absolute",
-        left: 16,
-        top: 16,
-        zIndex: 99,
-      }}
-      onClick={cb}
-    >
-      &#xe603;
-    </i>
-  );
-};
-const Title = ({ name, date, children }) => {
+import { Collapse, Row, Tabs } from "antd";
+const { TabPane } = Tabs;
+
+const Title = ({ name, date, cb, children }) => {
   return (
     <div className={styles.title}>
-      <p className={styles.name}>
+      <p style={{ marginTop: 20 }}>
+        <i
+          className={
+            globalStyle.global_icon + ` ${styles.gobackBtn} ${globalStyle.btn}`
+          }
+          onClick={cb}
+        >
+          &#xe603;
+        </i>
+      </p>
+      <p className={styles.name} style={{ marginTop: 109 }}>
         <span>{name}</span>
       </p>
-      <p className={styles.date}>
+      <p
+        className={styles.date}
+        style={{
+          marginTop: 5,
+        }}
+      >
         <span>{date}</span>
       </p>
-      <div style={{
-        position: 'absolute',
-        left: '10px',
-        bottom: '0px',
-        fontSize: '20px',
-        height: '30px',
-        display: 'flex',
-      }}>
-      <i className={globalStyle.global_icon + ` ${globalStyle.btn}`}>&#xe65f;</i>
-      <div className={styles.tab + ` ${styles.slect} ${globalStyle.btn}`}>按区域</div>
-      <div className={styles.tab + ` ${globalStyle.btn}`}>按标签</div>
+      <p
+        style={{
+          marginTop: 8,
+        }}
+      >
+        <i className={globalStyle.global_icon + ` ${globalStyle.btn}`}>
+          &#xe65f;
+        </i>
+        {children}
+      </p>
+    </div>
+  );
+};
+
+const ScoutingItem = ({ data }) => {
+  const header = (
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
+      <div className={styles.numberIcon}>
+        <span>2</span>
+      </div>
+      <div className={styles.text}>
+        <span>区域A的调研</span>
+      </div>
+    </div>
+  );
+  return (
+    <Collapse expandIconPosition="right" className={styles.scoutingItem}>
+      <Collapse.Panel header={header}>
+        <div className={styles.itemDetail}>
+          <p>
+            <i className={globalStyle.global_icon}>&#xe616;</i>
+            <span>3月15日</span>
+            <i className={globalStyle.global_icon}>&#xe600;</i>
+            <span>沙寮村委</span>
+          </p>
+          <p>
+            <i className={globalStyle.global_icon}>&#xe685;</i>
+            <span>执行任务清单、备忘、要求等细节说明填在此</span>
+          </p>
+        </div>
+        <UploadItem type="paper" />
+        <UploadItem type="paper" />
+        <UploadItem type="interview" />
+        <UploadItem type="pic" />
+        <UploadItem type="video" />
+        <UploadItem type="word" />
+        <UploadItem type="annotate" />
+        <UploadItem type="plotting" />
+        <UploadItem type="video" />
+        <i
+          className={globalStyle.global_icon + ` ${globalStyle.btn}`}
+          style={{ fontSize: 30, color: "#0D4FF7" }}
+        >
+          &#xe628;
+        </i>
+      </Collapse.Panel>
+    </Collapse>
+  );
+};
+const ScoutingItem2 = ({ data }) => {
+  const header = (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <div style={{ textAlign: "left" }}>
+        <i className={globalStyle.global_icon + ` ${globalStyle.btn}`}>
+          &#xe6d9;
+        </i>
+        人口
+      </div>
+      <i
+        className={globalStyle.global_icon + ` ${globalStyle.btn}`}
+        style={{ fontSize: 30, color: "#0D4FF7" }}
+      >
+        &#xe628;
+      </i>
+    </div>
+  );
+  return (
+    <Collapse expandIconPosition="right" className={styles.scoutingItem}>
+      <Collapse.Panel header={header}>
+        <UploadItem type="paper" />
+        <UploadItem type="paper" />
+        <UploadItem type="interview" />
+      </Collapse.Panel>
+    </Collapse>
+  );
+};
+const UploadItem = ({ type }) => {
+  const itemKeyVals = {
+    paper: "图纸",
+    interview: "访谈",
+    pic: "图片",
+    video: "视频",
+    word: "文档",
+    annotate: "批注",
+    plotting: "标绘",
+  };
+  return (
+    <div className={styles.uploadItem}>
+      <div className={styles.uploadIcon + ` ${styles[type]}`}>
+        <span>{itemKeyVals[type]}</span>
+      </div>
+      <div className={styles.uploadDetail}>
+        <Row>
+          <span>可以叠加到地图上的资料</span>
+        </Row>
+        <Row>
+          <span>区珊</span>
+          <span>2020/01/27</span>
+          <span>05:30PM</span>
+        </Row>
       </div>
     </div>
   );
@@ -52,6 +160,7 @@ export default class ScoutingDetails extends PureComponent {
     this.state = {
       name: "阳山县沙寮村踏勘",
       date: "3/15-3/17",
+      visible: true,
     };
   }
   handleGoBackClick = () => {
@@ -63,14 +172,49 @@ export default class ScoutingDetails extends PureComponent {
       },
     });
   };
+  handleTogglePanelClick = (index) => {};
   render(h) {
     const { name, date } = this.state;
+    const panelStyle = {
+      height: "93%",
+    };
     return (
       <div className={styles.wrap}>
-        <GoBack cb={this.handleGoBackClick.bind(this)}></GoBack>
-        <Title name={name} date={date}>
-
-        </Title>
+        <Title
+          name={name}
+          date={date}
+          cb={this.handleGoBackClick.bind(this)}
+        ></Title>
+        <Tabs
+          // tabPosition="left"
+          tabBarGutter={10}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            overflow: "hidden",
+            position: "absolute",
+            top: 196,
+            left: 0,
+            bottom: 2,
+            width: "100%"
+          }}
+        >
+          <TabPane tab={<span>按区域</span>} key="1" style={panelStyle}>
+            <div className={globalStyle.autoScrollY} style={{ height: "100%" }}>
+              <ScoutingItem />
+              <ScoutingItem />
+              <ScoutingItem />
+              <ScoutingItem />
+            </div>
+          </TabPane>
+          <TabPane tab={<span>按标签</span>} key="2">
+            <div className={globalStyle.autoScrollY} style={{ height: "100%" }}>
+              <ScoutingItem2 />
+              <ScoutingItem2 />
+              <ScoutingItem2 />
+            </div>
+          </TabPane>
+        </Tabs>
       </div>
     );
   }
