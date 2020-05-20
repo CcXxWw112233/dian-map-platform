24f05c58618b36b8e5239996a8dbf84a643ff73d
hejt <robbie528>
2020-05-20 17:19:28 +0800

    添加分组数据等增删改查功能

--- src/components/PublicOverlays/baseOverlay/index.js
+++ src/components/PublicOverlays/baseOverlay/index.js
@@ -12,11 +12,31 @@ export default function baseOverlay(content, data = {}){
         div.classList.add(data.className);
     }
 
+    let position = (placement) => {
+        switch(placement){
+            case "bottomLeft" :  ;
+            case "bottomCenter" : ;
+            case "bottomRight" : 
+                return "5,0,10,10,20,0"
+            ;
+            default :
+            return "5,0,10,10,20,0";
+        }
+    }
+
     // 角
     let span = document.createElement('span');
     span.className = styles.baseOverlayAngle;
     span.classList.add(this.placement);
-    span.style.borderColor = this.bgColor;
+    Object.assign(span.style, data.angleStyle);
+    // span.style.borderColor = this.bgColor;
+    span.innerHTML = `<svg xmlns="https://www.w3.org/2000/svg" width="20px" height="10px" version="1.1">
+        <polyline 
+        points=${position(data.placement)} 
+        style="fill:${data.angleColor || "#1769FF"}; 
+        stroke:${data.angleBorderColor || "#ffffff"};
+        stroke-width:2" />
+    </svg>`
     // 角
 
     typeof content === 'string' ? div.innerHTML = content : content instanceof HTMLElement ? div.appendChild(content) : "";
