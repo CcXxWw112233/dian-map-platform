0d74f81d774b417710fb4c22e01ab8660e837121
huangxd <huangxd@new-di.com>
2020-05-30 10:12:22 +0800

    :bug: 支持apphttp\https混合加载

--- src/utils/mapSource.js
+++ src/utils/mapSource.js
@@ -5,50 +5,50 @@ const baseMaps = [
     title: "高德电子图",
     id: "gd_vec_tile",
     url:
-      "http://webrd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}",
+      `${document.location.protocol}//webrd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}`,
   },
   {
     title: "高德影像图",
     id: "gd_img_tile",
     url:
-      "http://webst0{1-4}.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}",
+      `${document.location.protocol}//webst0{1-4}.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}`,
   },
   {
     title: "高德路线图",
     id: "gd_roadLabel_tile",
     url:
-      "http://webst0{1-4}.is.autonavi.com/appmaptile?style=8&x={x}&y={y}&z={z}",
+      `${document.location.protocol}//webst0{1-4}.is.autonavi.com/appmaptile?style=8&x={x}&y={y}&z={z}`,
   },
   {
     title: "谷歌遥感图",
     id: "gg_img_tile",
     url:
-      "http://mt2.google.cn/vt/lyrs=y&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}&s=G",
+      `${document.location.protocol}//mt2.google.cn/vt/lyrs=y&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}&s=G`,
   },
   {
     title: "天地图影像图",
     id: "td_img_tile",
-    url: `http://t{0-7}.tianditu.gov.cn/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=${baseConfig.TIANDITU_APP_KEY}`,
+    url: `${document.location.protocol}//t{0-7}.tianditu.gov.cn/DataServer?T=img_w&x={x}&y={y}&l={z}&tk=${baseConfig.TIANDITU_APP_KEY}`,
   },
   {
     title: "天地图网路图",
     id: "td_roadLabel_tile",
-    url: `http://t{0-7}.tianditu.gov.cn/DataServer?T=cta_w&x={x}&y={y}&l={z}&tk=${baseConfig.TIANDITU_APP_KEY}`,
+    url: `${document.location.protocol}//t{0-7}.tianditu.gov.cn/DataServer?T=cta_w&x={x}&y={y}&l={z}&tk=${baseConfig.TIANDITU_APP_KEY}`,
   },
   {
     title: "天地图矢量图",
     id: "td_vec_tile",
-    url: `http://t{0-7}.tianditu.gov.cn/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=${baseConfig.TIANDITU_APP_KEY}`,
+    url: `${document.location.protocol}//t{0-7}.tianditu.gov.cn/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=${baseConfig.TIANDITU_APP_KEY}`,
   },
   {
     title: "天地图地形图",
     id: "td_ter_tile",
-    url: `http://t{0-7}.tianditu.gov.cn/DataServer?T=ter_w&x={x}&y={y}&l={z}&tk=${baseConfig.TIANDITU_APP_KEY}`,
+    url: `${document.location.protocol}//t{0-7}.tianditu.gov.cn/DataServer?T=ter_w&x={x}&y={y}&l={z}&tk=${baseConfig.TIANDITU_APP_KEY}`,
   },
   {
     title: "天地图注记",
     id: "td_label_tile",
-    url: `http://t{0-7}.tianditu.gov.cn/DataServer?T=cva_w&x={x}&y={y}&l={z}&tk=${baseConfig.TIANDITU_APP_KEY}`,
+    url: `${document.location.protocol}//t{0-7}.tianditu.gov.cn/DataServer?T=cva_w&x={x}&y={y}&l={z}&tk=${baseConfig.TIANDITU_APP_KEY}`,
   },
 ];
 const baseMapKeys = [
