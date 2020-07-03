beb2d24f4b2fc25431bebbb55e141fbea0fdf955
huangxd <huangxd@new-di.com>
2020-07-03 16:30:19 +0800

    :bug:

--- src/utils/plotEdit.js
+++ src/utils/plotEdit.js
@@ -130,7 +130,7 @@ export const plotEdit = {
           JSON.stringify(JSON.parse(operator.data.content)?.coordinates)
         ) {
           const data = operator.data;
-          delete data.sort;
+          data.sort && delete data.sort;
           operator.updateFeatueToDB(data, feature).then((res) => {
             Event.Evt.firEvent("updatePlotFeature", res);
           });
