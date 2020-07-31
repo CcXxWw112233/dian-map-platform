f6fbc66d3521a645dce6a65db044b110cc1d1947
hejt <hejt@new-di.com>
2020-07-31 21:52:13 +0800

    优化放大缩小的交互

--- src/components/Zoom/index.js
+++ src/components/Zoom/index.js
@@ -16,6 +16,9 @@ export default class Zoom extends React.Component {
     this.moving = false;
     this.lastY = null;
     this.defaultZoom = 12;
+    this.startEvent = null;
+    // 缩放比例
+    this.zoomPixo = 0.15;
     // window.onmouseup = (e) => this.onMouseUp(e);
     // window.onmousemove = (e) => this.onMouseMove(e);
     window.onpointerup = (e) => this.onMouseUp(e);
@@ -31,6 +34,12 @@ export default class Zoom extends React.Component {
     e.preventDefault();
     e.stopPropagation();
     this.moving = true;
+    // 记录第一次触摸
+    this.startEvent = this.transfromClient(e);
+  }
+
+  transfromClient = (client)=>{
+    return {x: client.clientX, y: client.clientY}
   }
 
   onMouseUp() {
@@ -48,41 +57,67 @@ export default class Zoom extends React.Component {
   }
 
   onMove(e) {
-    if (this.lastY) {
-      let dy = e.clientY - this.lastY;
-      let zoom = mapApp.map.getView().getZoom();
-      if (dy > 130 || dy < -120) {
-        this.setState(
-          {
-            translateY: dy > 0 ? 0 : -250,
-          },
-          () => {
-            if (dy > 130) {
-              zoom = mapApp.map.getView().getMinZoom();
-            } else if (dy < -120) {
-              zoom = mapApp.map.getView().getMaxZoom();
-            }
-            mapApp.map.getView().setZoom(zoom);
-          }
-        );
-      } else {
-        this.setState(
-          {
-            translateY: dy - 115,
-          },
-          () => {
-            if (dy > 0) {
-              let minZoom = mapApp.map.getView().getMinZoom();
-              zoom = zoom - dy / (130 / (zoom - minZoom));
-            } else if (dy < 0) {
-              let maxZoom = mapApp.map.getView().getMaxZoom();
-              zoom = zoom + dy / (-120 / (maxZoom - zoom));
-            }
-          }
-        );
-      }
-      mapApp.map.getView().setZoom(zoom);
+    // let { translateY } = this.state;
+    let moveClient = this.transfromClient(e);
+    let step = this.startEvent.y - moveClient.y;
+    this.startEvent = moveClient;
+    // console.log(this.startEvent.y - moveClient.y);
+    // step 大于0 放大
+    if(Math.abs(this.state.translateY) >= 250  && step > 0){
+      return ;
     }
+    if(this.state.translateY >= 0 && step < 0){
+      return ;
+    }
+
+    this.setState({
+      translateY: this.state.translateY + (-step)
+    })
+    
+    // step < 0 说明向下移动，是缩小， 反之
+    let zoom = mapApp.map.getView().getZoom();
+    if(step > 0){
+      zoom += this.zoomPixo;
+    }else{
+      zoom -= this.zoomPixo;
+    }
+    mapApp.map.getView().setZoom(zoom);
+
+    // if (this.lastY) {
+    //   let dy = e.clientY - this.lastY;
+    //   let zoom = mapApp.map.getView().getZoom();
+    //   if (dy > 130 || dy < -120) {
+    //     this.setState(
+    //       {
+    //         translateY: dy > 0 ? 0 : -250,
+    //       },
+    //       () => {
+    //         if (dy > 130) {
+    //           zoom = mapApp.map.getView().getMinZoom();
+    //         } else if (dy < -120) {
+    //           zoom = mapApp.map.getView().getMaxZoom();
+    //         }
+    //         mapApp.map.getView().setZoom(zoom);
+    //       }
+    //     );
+    //   } else {
+    //     this.setState(
+    //       {
+    //         translateY: dy - 115,
+    //       },
+    //       () => {
+    //         if (dy > 0) {
+    //           let minZoom = mapApp.map.getView().getMinZoom();
+    //           zoom = zoom - dy / (130 / (zoom - minZoom));
+    //         } else if (dy < 0) {
+    //           let maxZoom = mapApp.map.getView().getMaxZoom();
+    //           zoom = zoom + dy / (-120 / (maxZoom - zoom));
+    //         }
+    //       }
+    //     );
+    //   }
+    //   mapApp.map.getView().setZoom(zoom);
+    // }
     // this.lastY = e.clientY;
   }
 
@@ -94,6 +129,8 @@ export default class Zoom extends React.Component {
         <div
           className={styles.btn}
           style={{
+            userSelect:'none',
+            touchAction:"none",
             transform: `translateY(${this.state.translateY}px)`,
           }}
           ref="zoomBtn"
@@ -104,11 +141,13 @@ export default class Zoom extends React.Component {
             });
             // this.lastY = document.body.clientHeight - 500;
           }}
-          onPointerLeave={() => {
+          onPointerLeave={(e) => {
             if (!this.moving) {
               this.setState({
                 showSlider: false,
               });
+            }else{
+              // this.onMouseUp(e)
             }
           }}
         >
