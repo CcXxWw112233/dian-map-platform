aaaba6a52509fc9c22b353e30084d335519150ec
hejt <hejt@new-di.com>
2021-03-05 19:08:55 +0800

    修复打包报错

---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+// const CompressionPlugin = require("compression-webpack-plugin");
+// const BabelEnginePlugin = require("babel-engine-plugin");
+
+module.exports = (config, { webpack }) => {
+  return config;
+};
