f16bd6f80cd232635f08db8c405b9d79f631feba
hejt <hejt@new-di.com>
2021-03-11 10:28:43 +0800

    更新packagejson

--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-// const CompressionPlugin = require("compression-webpack-plugin");
-// const BabelEnginePlugin = require("babel-engine-plugin");
-
-module.exports = (config, { webpack }) => {
-  return config;
-};
